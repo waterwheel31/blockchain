29dac982c919a98096e66a678522d929bde55cd7
pairstudy <waterwheel31@gmail.com>
2018-10-09 14:06:28 +0900

    add everything

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const app = express();
-const simpleChain = require('simpleChainM.js');
+const simpleChain = require('simpleChainM');
+const port = 8000;
 
 const blockChain = new simpleChain.Blockchain();
 
 app.get('/', function (req, res) {
-	res.send('hello world!'); 
-
 	let blockHeight = blockChain.showBlockHeight();
-	res.send(blockHeight);
+	console.log(blockHeight);
+	console.log(blockChain);
+	res.write('Initial Block is:');
+	res.write(JSON.stringify(blockChain));
+	
+	res.end();
 });
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'));
+app.get('/block/:id(\\d+)', function (req, res) {
+	
+	let inputId  = req.params.id;
+	res.write('block #'+inputId+ '=');	
+	blockChain.getBlock(inputId).then(function(block){
+		res.write(JSON.parse(JSON.stringify(block)));
+		res.end();
+	});
+	
+	
+});
+
+app.get('/block', function (req, res) {
+	
+	res.write('last block = ');	
+	res.end();
+
+});
+
+
+app.listen(port, () => console.log('Example app listening on port '+ port + '!'));
